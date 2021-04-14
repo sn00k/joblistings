1654b8526d763e648b9264e832737ef5ee47a4f2
Robin Nilsson <robin@robins.nu>
2021-04-14 22:26:43 +0200

    wip: jobs styling

--- components/Jobs.js
+++ components/Jobs.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CenteredCard from './CenteredCard'
-import { Grid } from '@material-ui/core'
+import { Grid, Typography, Link, Box } from '@material-ui/core'
 
 export default function Jobs({ jobs }) {
   return jobs ? jobs.map((job) => <JobItem job={job} key={job.slug} />) : null
@@ -16,11 +16,41 @@ function JobItem({ job }) {
           spacing={3}
         >
           <Grid item xs={12}>
-            <p>Title: {job.title}</p>
-            <p>Description: {job.description}</p>
+            <Box display="flex" flexDirection="column">
+              <Typography variant="body2">
+                <Link href={`/post/${job.slug}`}>
+                  {job.title}
+                </Link>
+              </Typography>
+              <Box display="flex">
+                <Box flexGrow="1" color={job.position === 'fullTime' ? 'success.main' : 'inherit'}>
+                  <Typography variant="caption" flexGrow="1">
+                    {printPositionType(job.position)}
+                  </Typography>
+                </Box>
+                <Typography variant="caption">
+                  {job.location}
+                </Typography>
+                <Typography variant="caption">
+                  8 hours ago
+                </Typography>
+              </Box>
+            </Box>
           </Grid>
         </Grid>
       </CenteredCard>
     </>
   )
+}
+
+function printPositionType(position) {
+  switch (position) {
+    case 'fullTime':
+      return 'Full Time'
+    case 'partTime':
+      return 'Part Time'
+    case 'contract':
+      return 'Contract'
+    default: return 'Full Time'
+  } 
 }
